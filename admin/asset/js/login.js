75d48b58c463da130818e3a6b9625fa69bcbfe18
Wong Chun Wai <damon@damonw.com>
2015-11-17 01:13:50 +0800

    Bug fix for google api of mobile login

--- admin/asset/js/login.js
+++ admin/asset/js/login.js
@@ -2,10 +2,28 @@ $(document).ready(function(){
 	$("#loginbtn").click(function(){
 		var user=$("#username").val();
 		var pass=$("#pwd").val();
-		var countryname=google.loader.ClientLocation.address.country;
-		var latitude=google.loader.ClientLocation.latitude;
-		var longitude=google.loader.ClientLocation.longitude;
-		if(user.length>0&&pass.length>0){
+		if(google.loader.ClientLocation==null){
+			if(user.length>0&&pass.length>0){
+			$.ajax({
+				type:"POST",
+				data:"username="+user+"&pwd="+pass+"&countryname=null&latitude=null&longitude=null",
+				url:"asset/cklogin.php",
+				success:function(html){
+					if(html=='true'){
+							window.location='index.php';
+					}else{
+						$("#message").html('<div class="alert alert-danger"><strong>Error!</strong>Wrong Username or Password</div>');
+					}
+				}
+			});
+			}else{
+				$("#message").html('<div class="alert alert-danger"><strong>Error!</strong> Please fill in Username and Password!</div>');
+			}	
+		}else{
+			if(user.length>0&&pass.length>0){
+			var countryname=google.loader.ClientLocation.address.country;
+			var latitude=google.loader.ClientLocation.latitude;
+			var longitude=google.loader.ClientLocation.longitude;
 			$.ajax({
 				type:"POST",
 				data:"username="+user+"&pwd="+pass+"&countryname="+countryname+"&latitude="+latitude+"&longitude="+longitude,
@@ -21,5 +39,7 @@ $(document).ready(function(){
 		}else{
 			$("#message").html('<div class="alert alert-danger"><strong>Error!</strong> Please fill in Username and Password!</div>');
 		}	
+		}
+		
 	})
 })
