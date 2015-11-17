f904e454a97c501d2ff689d3fb9b687aa95f438d
Wong Chun Wai <damon@damonw.com>
2015-11-17 17:16:46 +0800

    Added User log

--- admin/asset/navbar.php
+++ admin/asset/navbar.php
@@ -3,6 +3,7 @@
 		echo '<li><a href="index.php"><span class="glyphicon glyphicon-home"></span> Home</a></li>
 				<li><a href="admin.php"><span class="glyphicon glyphicon-user"></span> Administrator</a></li>
 				<li><a href="adminlog.php"><span class="glyphicon glyphicon-record"></span> Admin Logging</a></li>
+				<li><a href="userlog.php"><span class="glyphicon glyphicon-record"></span> User Logging</a></li>
 		        <li><a href="user.php"><span class="glyphicon glyphicon-user"></span> User</a></li>
 		        <li><a href="music.php"><span class="glyphicon glyphicon-headphones"></span> Music</a></li>
 		        <li><a href="adminmessage.php"><span class="glyphicon glyphicon-envelope"></span> Admin Message</a></li>';
